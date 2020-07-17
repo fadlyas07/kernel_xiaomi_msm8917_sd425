7d960f73f59e14b71b6f0373b78b8a8961292a90
fadlyas07 <fadlyardhians@gmail.com>
2020-07-17 15:33:37 +0700

    touchscreen/FT5346: fix misleading indentation warning
    ../drivers/input/touchscreen/FT5346/ft5346.c:2361:3: warning: misleading indentation; statement is not part of the previous 'else' [-Wmisleading-indentation]
                    err = hardwareinfo_set_prop(HARDWARE_TP, firmware_ver);
                    ^
    ../drivers/input/touchscreen/FT5346/ft5346.c:2358:4: note: previous statement is here
            } else
              ^
    
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>
    Change-Id: I85ae215f300cba9c80190f499b5d546dcf8f041e

--- drivers/input/touchscreen/FT5346/ft5346.c
+++ drivers/input/touchscreen/FT5346/ft5346.c
@@ -2355,14 +2355,14 @@ static int hardwareinfo_set(struct ft5x06_ts_data *data, u8 value_name, u8 color
 		reg_addr = 0xA6;
 		ft5x06_i2c_read(data->client, &reg_addr, 1, &uc_tp_fm_ver, 1);
 		snprintf(firmware_ver, HARDWARE_MAX_ITEM_LONGTH, "%s, %s, FW:0x%x, %s", vendor_for_id, ic_name, uc_tp_fm_ver, ic_color);
-	} else
-		snprintf(firmware_ver, HARDWARE_MAX_ITEM_LONGTH, "%s, %s, FW:0x%x, %s", vendor_for_id, ic_name, data->fw_ver[0], ic_color);
-
+	} else {
 		err = hardwareinfo_set_prop(HARDWARE_TP, firmware_ver);
 		dev_err(&data->client->dev, "Set hardwareinfo is: %s\n", firmware_ver);
-		if (err < 0)
+		snprintf(firmware_ver, HARDWARE_MAX_ITEM_LONGTH, "%s, %s, FW:0x%x, %s", vendor_for_id, ic_name, data->fw_ver[0], ic_color);
+		if (err < 0) {
 		return -EPERM;
-
+		}
+	}
 	return 0;
 }
 #endif
